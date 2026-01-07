3be13bbe0377ffb83a0189773e94f80ddf8b147d
Rocool45 <kulkarnirohan32@gmail.com>
2026-01-07 16:13:42 +0530

    fixed role

--- src/app/shared/service/userRole.gaurd.ts
+++ src/app/shared/service/userRole.gaurd.ts
@@ -1,26 +1,16 @@
-import { inject, Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
-import { SnackbarService } from "./snackbar.service";
 
 @Injectable({
     providedIn:"root"
 })
 export class UserRoleGuard implements CanActivate{
-    private router = inject(Router)
-    private sncakbar =inject(SnackbarService)
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
      let userROle :Array<string> =   route.data["userRole"]
 
      let Loginedin:string = localStorage.getItem("userRole")!
 
-     if(userROle.includes(Loginedin)){
-         return userROle.includes(Loginedin)
-
-     }else{
-      
-        return this.router.createUrlTree([""])
-     }
-
+     return userROle.includes(Loginedin)
     }
 }
